9da5063890a97060b8d1af1e10c0c03c7a44edd5
ahmadkhaleel96 <ahmadkhaleel96@yahoo.com>
2019-12-03 10:14:04 +0200

    added the css fot the form and debugged the fat, carbs and protien funtion

--- js/app.js
+++ js/app.js
@@ -1,5 +1,6 @@
 'use strict';
 //  Array
+var calories = 0 ;
 var Fitness = [];
 var workout = [];
 var quantities = [];
@@ -7,13 +8,13 @@ var form = document.getElementById('fitPlan');
 var sectionEl = document.getElementById('response');
 
 //  Constructor
-function Plan(name, age, weight, height, activitiesHours) {
+function Plan(name, age, weight, height, activitiesHours, target) {
   this.name = name;
   this.age = age;
   this.weight = weight;
   this.height = height;
   this.activitiesHours = activitiesHours;
-  // this.target = target;
+  this.target = target;
   this.perfectWeight = 0;
   this.caloriesPerDay = 0 ;
   Fitness.push(this);
@@ -21,6 +22,7 @@ function Plan(name, age, weight, height, activitiesHours) {
   this.suitableWeight();
   this.rateCalculate();
   this.programWorkOut();
+  // console.log(this.target)
 }
 
 // Function for caloriesCal by the activities Hours and weight
@@ -43,14 +45,14 @@ Plan.prototype.caloriesCalculation = function () {
 // Function for workout by age and calories
 Plan.prototype.programWorkOut = function () {
   if ( this.age < 18) {
-    workout.push('Each exercise 3 sets and 10 reps,Barbell Squat, ABS sit-up,Chest butterfly,Leg press,Push-ups,Plank "30sec",Lunge');
+    workout.push('Each exercise 3 sets and 10 reps','Barbell Squat', 'ABS sit-up,Chest butterfly','Leg press','Push-ups','Plank "30sec"','Lunge');
   }
   else if (18 <= this.age < 40) {
-    workout.push('Barbell Squat, ABS sit-up,Chest butterfly,Leg press,Push-ups,Plank "60sec",Lunge');
+    workout.push('Barbell Squat', 'ABS sit-up','Chest butterfly','Leg press','Push-ups','Plank "60sec"','Lunge');
   } else if (40 <= this.age) {
-    workout.push('Barbell Squat, ABS sit-up,Chest butterfly,Leg press,Push-ups,Plank "25sec",Lunge');
+    workout.push('Barbell Squat', 'ABS sit-up','Chest butterfly','Leg press','Push-ups','Plank "25sec"','Lunge');
   }
-
+// console.log(workout)
 };
 
 
@@ -63,43 +65,53 @@ Plan.prototype.suitableWeight = function() {
   // this.perfectWeight = 2.2 * bmi + (3.5 * bmi) * (this.height - 1.5);
   // console.log(this.perfectWeight);
 };
-
+// console.log('hi',workout)
 
 var lose;
 var gain;
 var maintenance;
-var Protein;
-var carbs;
-var fats;
-var calories = 0 ;
+var Protein=0;
+var carbs=0;
+var fats=0;
+
+// console.log('calories', calories)
 // function to calculate the Protein, carbohydrates and fats
+// var x;
 Plan.prototype.rateCalculate = function () {
+  // x = 10 ; 
+  // console.log(x);
   // this if statemennt work if the lose checkbox of the form is checked.
-  if (this.target === lose){
-    calories = this.caloriesPerDay - 200;
-    Protein = (calories*0.35)/4;
-    carbs = (calories*0.35)/4;
-    fats = (calories*0.15)/9;
+  // console.log(this.target);
+  if (this.target == 'lose' || this.target == 'l'){
+    calories = this.caloriesPerDay  - 200;
+    Protein = (calories*0.45)/6;
+    carbs = (calories*0.45)/6;  
+    fats = (calories*0.10)/9;
+  //  console.log(Protein,carbs,fats)
   }
   // this is work when the gain checkbox of the form is checked.
-  if (this.target === gain){
+ else if (this.target == 'gain' || this.target == 'g'){
     calories = this.caloriesPerDay +200 ;
-    Protein = (calories*0.25)/4 ;
-    carbs = (calories*0.5)/4 ;
+    Protein = (calories*0.25)/6 ;
+    carbs = (calories*0.5)/6;
     fats =  (calories*0.25)/9 ;
   }
   // this is work when the maintenance checkbox of the form is checked.
-  if (this.target === maintenance){
+   else if (this.target == 'maintenance' || this.target == 'm'){
     calories = this.caloriesPerDay ;
-    Protein = (calories*0.5)/4;
-    carbs = (calories*0.4)/4;
+    Protein = (calories*0.5)/6;
+    carbs = (calories*0.4)/6;
     fats = (calories*0.1)/9;
   }
+  
+  // console.log(this.target) 
   // this array is the data input for the pie chart that we use to show the values needed on a daily bases.
   quantities.push(Protein, carbs, fats);
   console.log(Protein);
   console.log(carbs);
   console.log(fats);
+  // console.log('p', Protein, 'c', carbs, 'f', fats);
+  // console.log(quantities)
 };
 
 function handleSubmit(event) {
@@ -114,26 +126,49 @@ function handleSubmit(event) {
 
 
   var activitiesHours = parseInt(event.target.activitiesHours.value);
-  // var target = event.target.target.value;
-  var newplan = new Plan(name,age,weight,height,activitiesHours,);
+  var target = event.target.target.value;
+  var newplan = new Plan(name,age,weight,height,activitiesHours, target);
   newplan.render();
 }
+
+
+
+// caloriesCalculation();
+// suitableWeight();
+// rateCalculate();
+// programWorkOut();
 // console.log(this.target.lose.checked);
 form.addEventListener('submit', handleSubmit);
 
 //function for rendering
 Plan.prototype.render= function(){
+  var pEl = document.createElement('p');
+  sectionEl.appendChild(pEl);
+  pEl.textContent = `Nice to meet you ${this.name}.` 
+
+  var pEl = document.createElement('p');
+  sectionEl.appendChild(pEl);
+  pEl.textContent = `Depending on your information you need ${this.caloriesPerDay} calories a day`
+  
+  var pEl = document.createElement('p');
+  sectionEl.appendChild(pEl);
+  pEl.textContent = `Because you worked ${this.activitiesHours} hour a day.` 
+
+  var pEl = document.createElement('p');
+  sectionEl.appendChild(pEl);
+  pEl.textContent = `your perfect weight should be ${this.perfectWeight} kg.` 
+  
+  var pEl = document.createElement('p');
+  sectionEl.appendChild(pEl);
+  pEl.textContent = `To show you how much we love you and support your dicision here is our free workout for you ${this.name}.`
+  for (var i=0; i <= workout.length-1; i++){
   var liEl = document.createElement('li');
   sectionEl.appendChild(liEl);
-  // console.log(Fitness.name);
-  // console.log(Plan.caloriesPerDay);
-  liEl.textContent = `Nice to meet you ${this.name} Depend on your information you need ${Math.floor(this.caloriesPerDay)} calories daily , because you worked ${this.activitiesHours} hour/day, your perfect weight should be 
-  ${this.perfectWeight}kg`;
-  var li2El = document.createElement('li');
-  sectionEl.appendChild(li2El);
-  li2El.textContent= `Enjoy Our Free workOut Program: ${workout}`;
-
+  liEl.textContent= `${workout[i]}`;
+  }
 };
+// console.log(Fitness.name);
+// console.log(Plan.caloriesPerDay);
 
 
 
